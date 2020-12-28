009bd116140ffe3fb90911fb6432f6e37e425d61
Julia Philipp <julia@Julias-MacBook-Pro.local>
2020-12-28 10:08:30 -0800

    add course materials from matt. wip. some files too big

---
+++ Data_Normalization_and_Scaling/initLesson.R
@@ -0,0 +1,15 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+if (!requireNamespace("Seurat", quietly = TRUE))
+  BiocManager::install("Seurat")
+
+load(file.path(.get_course_path(), "scRNA-seq", "Data_Normalization_and_Scaling","pbmc.rds"))
