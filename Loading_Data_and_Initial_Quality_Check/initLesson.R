32530d30d84fa385c195323d470f751e7feb8caf
Almas <khnalmas@gmail.com>
2021-07-24 13:29:22 -0700

    init lesson barcodes.tsv.gz

--- Loading_Data_and_Initial_Quality_Check/initLesson.R
+++ Loading_Data_and_Initial_Quality_Check/initLesson.R
@@ -12,4 +12,4 @@
 if (!requireNamespace("Seurat", quietly = TRUE))
   BiocManager::install("Seurat")
 
-barcodes.tsv <- file.path(.get_course_path(), 'BiocSwirl_scRNAseq', 'barcodes.tsv.gz')
+barcodes.tsv.gz <- file.path(.get_course_path(), 'BiocSwirl_scRNAseq', 'barcodes.tsv.gz')
